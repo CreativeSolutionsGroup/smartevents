96955b7f80ec375571a2bd0f7295c7c3fd3208f4
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-09-18 14:37:24 -0400

    fixed new user creation when signing in

--- lib/server/event/update.ts
+++ lib/server/event/update.ts
@@ -15,16 +15,21 @@ export async function updateEvent(id: string, data: Partial<Event>) {
 }
 
 export async function attendEvent(eventId: string, userEmail: string) {
-  console.log("attendEvent called with:", { eventId, userEmail });
-
-  const user = await prisma.authorizedUser.findUnique({
+  let user = await prisma.authorizedUser.findUnique({
     where: { email: userEmail },
   });
 
-  console.log("user found:", user);
+  if (!user) {
+    user = await prisma.authorizedUser.create({
+      data: {
+        email: userEmail,
+        role: "VIEWER",
+        user: { connect: { email: userEmail } },
+      },
+    });
+  }
 
   if (!user) {
-    console.log("User not found, redirecting to sign-in");
     redirect(`/event/${eventId}/attend`);
   }
 
@@ -37,31 +42,23 @@ export async function attendEvent(eventId: string, userEmail: string) {
     },
   });
 
-  console.log("existingAttendance:", existingAttendance);
-
   if (!existingAttendance) {
     const event = await prisma.event.findUnique({
       where: { id: eventId },
     });
 
-    console.log("event found:", event);
-
     if (!event) {
-      console.log("Event not found, redirecting to not-found");
       redirect("/event/not-found");
     }
 
     if (event.startTime && new Date(event.startTime) > new Date()) {
-      console.log("Event has not started yet, redirecting to too-early");
       redirect(`/event/${eventId}/too-early`);
     }
 
     if (event.endTime && new Date(event.endTime) <= new Date()) {
-      console.log("Event has ended, redirecting to too-late");
       redirect(`/event/${eventId}/too-late`);
     }
 
-    console.log("Creating new attendance record");
     await prisma.attendance.create({
       data: {
         eventId,
@@ -69,10 +66,8 @@ export async function attendEvent(eventId: string, userEmail: string) {
       },
     });
 
-    console.log("Attendance created successfully, redirecting to success");
     redirect(`/event/${eventId}/success`);
   }
 
-  console.log("User already attending event, redirecting to duplicate");
   redirect(`/event/${eventId}/duplicate`);
 }
